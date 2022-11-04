5a06f641d29895b7a0d11d1e448c924888002ea5
Vishva <gvishva2601@gmail.com>
2022-11-04 16:00:54 +0530

    Welcome Line Comparision Program

---
+++ src/com/LineComparision/WelcomeLineComparision.java
@@ -0,0 +1,10 @@
+package com.LineComparision;
+
+public class WelcomeLineComparision
+{
+    public static void main(String[] args)
+        {
+            System.out.println("welcome To Line Comparison Computation Program:");
+        }
+    }
+
